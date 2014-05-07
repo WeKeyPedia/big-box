4ebc526edfdcd8c6157267d981cba6aff9b158f4
taniki <tk@deveha.com>
2014-05-07 11:56:33 +0200

    remove worker as a subtree

--- apps/worker/src/tests/test_wikipedia_user.py
+++
@@ -1,8 +0,0 @@
-from wekeypedia.wikipedia_user import WikipediaUser as User
-
-def test_wikipedia_user_fetch_contributions():
-  u = User(name="DDima")
-
-  contribs = u.fetch_contribs()
-
-  assert len(contribs) > 500

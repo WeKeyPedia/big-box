5e8b20c78d261fddd09f08c3c780a5635b996783
taniki <tk@deveha.com>
2014-05-09 19:10:55 +0200

    make the nginx module more generic

--- apps/nginx-localhost/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/bin/sed -i "s/<api_addr>/${API_PORT_3000_TCP_ADDR}/" /etc/nginx/sites-available/default
-/bin/sed -i "s/<api_port>/${API_PORT_3000_TCP_PORT}/" /etc/nginx/sites-available/default
-nginx
